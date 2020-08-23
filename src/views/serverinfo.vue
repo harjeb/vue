8e25adb96b7a35b6e35ba0752ce0cb29d1eb6536
harjeb <harjeb@outlook.com>
2020-08-23 15:25:22 +0800

    fix

--- src/views/serverinfo.vue
+++ src/views/serverinfo.vue
@@ -9,7 +9,7 @@
         <Option v-for="item in moduleList" :value="item.value" :key="item.value">{{ item.label }}</Option>
         </Select>
         &nbsp;
-        <Button @click="check" ref="checkBtn">检查网络</Button>
+        <Button @click="getRes" ref="checkBtn">检查网络</Button>
         &nbsp;
         <Button @click="deleteP" type="error" style="width:60px" >删除</Button>
         <br>
@@ -71,36 +71,21 @@ export default {
         deleteP() {
             this.$emit('deleteIndex', this.index);
         },
-        check() {
-            console.log(config.flag)
-            this.$axios.get('http://127.0.0.1:5000/api/hi').then((response)=>{
-                    this.newsList=response.data.data;
-                    console.log(this.newsList);
-                }).catch((response)=>{
-                    console.log(response);
-                })
-            console.log('11')
-
-            if (this.server.ip.match(/^\s*$/) || this.server.user.match(/^\s*$/) || this.server.password.match(/^\s*$/)){
-                alert("没有填写信息")
-                this.$refs.checkBtn.$el.innerText="检查网络";
-                this.$refs.checkBtn.type= "default"
-                config.flag = false
-            }
-
-            else if (config.flag){
-                this.$refs.checkBtn.$el.innerText="检查网络";
-                this.$refs.checkBtn.type= "default"
-                config.flag = !config.flag
-            } else{
-                console.log(this.testmodule)
-                this.$refs.checkBtn.$el.innerText= "checking";
-                this.$refs.checkBtn.type= "success"
-                let latency=util.ssh_test(this.server.ip,this.server.user,this.server.password)
-                this.$refs.checkBtn.$el.innerText= latency;
-                config.flag = !config.flag
-                // TODO ssh连接后ping
-
+        //get latency
+        getLatency(){
+            return this.$axios.get('http://127.0.0.1:5000/api/hi')
+        },
+        async getRes(){
+            try {
+                if (this.server.ip.match(/^\s*$/) || this.server.user.match(/^\s*$/) || this.server.password.match(/^\s*$/)){
+                    alert("没有填写信息");
+                }
+                else{let ll = await this.getLatency();
+                console.log(ll.data);
+                this.$refs.checkBtn.$el.innerText=ll.data;}
+            } catch(err) {
+                console.log(err)
+                this.$refs.checkBtn.$el.innerText='failed';
             }
         }
 
